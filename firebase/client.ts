fc136d6606de5963c629019cae96ef6a2e9c563a
Michael Pollente <pollentem@gmail.com>
2025-04-09 22:18:47 +0800

    Move firebase config to env

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,16 +2,14 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDap0YriDePdFDuWJyoRRgcn5aVWXBDvcQ",
-  authDomain: "prepai-26655.firebaseapp.com",
-  projectId: "prepai-26655",
-  storageBucket: "prepai-26655.firebasestorage.app",
-  messagingSenderId: "244375186273",
-  appId: "1:244375186273:web:f91222bd1c9bb3ba3df320",
-  measurementId: "G-5QCNK7BGWH",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
