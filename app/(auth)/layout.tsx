cb60ede1dd1e83d808527cc249572983dcecab29
Michael Pollente <pollentem@gmail.com>
2025-04-09 21:03:29 +0800

    Add isAuthenticated check

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,12 @@
+import { redirect } from "next/navigation";
 import React, { ReactNode } from 'react';
+import { isAuthenticated } from "../../lib/actions/auth.action";
 
-const AuthLayout = ({children}: {children: ReactNode}) => {
+const AuthLayout = async ({children}: {children: ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) redirect("/");
+
   return (
     <div className="auth-layout">{children}</div>
   );
